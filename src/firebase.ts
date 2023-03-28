fde5253fdb24bbf9454462cab73c5e2a8263ce3c
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-03-28 19:36:36 +0300

    Add authentication

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-2H0yAjAiuoqm_Xg_Zeq1vrPQ1Sr1HrU",
+  authDomain: "cinasweeper.firebaseapp.com",
+  projectId: "cinasweeper",
+  storageBucket: "cinasweeper.appspot.com",
+  messagingSenderId: "669634161466",
+  appId: "1:669634161466:web:d835fc561feefa79b37e03"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+export const login = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+    return result;
+}
+export const logout = async () => {
+    await signOut(auth);
+}
--- src/main_page.tsx
+++ src/main_page.tsx
@@ -1,13 +1,33 @@
-import './main_page_styles.scss'
+import "./main_page_styles.scss";
+import { auth, login, logout } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 export default function MainPage() {
-    return (
-        <div className='mainpage'>
-            <div className='navbar'>log in</div>
-            <div className='logos'>
-                <div className='title1'>cin</div>
-                <div className='title2'>cin</div>
-            </div>
-            <div className='dscr'>mini-game about life at apps faculty in ucu</div>
-        </div>
-    )
+  const [user, loading] = useAuthState(auth);
+
+  return (
+    <div className="mainpage">
+      <div className="navbar">
+        {user ? (
+          <span onClick={logout}>
+            {user.photoURL && (
+              <img
+                src={user.photoURL}
+                referrerPolicy="no-referrer"
+                alt="user"
+              />
+            )}
+            <span>{user.displayName}</span>
+          </span>
+        ) : (
+          <span onClick={login}>Log in</span>
+        )}
+      </div>
+      <div className="logos">
+        <div className="title1">cin</div>
+        <div className="title2">cin</div>
+      </div>
+      <div className="dscr">mini-game about life at apps faculty in ucu</div>
+    </div>
+  );
 }
