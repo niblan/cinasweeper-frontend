c307c9dfd9ee1c27446b9c78c3e7185458ab94dc
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-04-08 12:16:12 +0300

    Add api url envvar

--- src/newgame.tsx
+++ src/newgame.tsx
@@ -1,34 +1,36 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 
 export default function Newgame() {
-    const api_url = "http://10.10.227.65:8000";
-    const [user, loading] = useAuthState(auth);
-    const navigate = useNavigate();
+  const api_url = import.meta.env.VITE_API_URL;
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
 
-    const createGame = async () => {
-        if(!user){return};
-        const jwt = user.accessToken;
-        const game = await fetch(`${api_url}/games`, {
-            method: 'POST',
-            headers: {
-                authorization: `Bearer ${jwt}`,
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-                gamemode: "1v1"
-            })
-        }).then(res => res.json());
-        console.log(game.identifier);
-        navigate(`/games/${game.identifier}`);
-    };
+  const createGame = async () => {
+    if (!user) {
+      return;
+    }
+    const jwt = user.accessToken;
+    const game = await fetch(`${api_url}/games`, {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${jwt}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        gamemode: "1v1",
+      }),
+    }).then((res) => res.json());
+    console.log(game.identifier);
+    navigate(`/games/${game.identifier}`);
+  };
 
-    useEffect(() => {createGame()}, []);
+  useEffect(() => {
+    createGame();
+  }, []);
 
-    return (
-        <div>
-        </div>
-    );
+  return <div></div>;
 }
+
--- src/top_games.tsx
+++ src/top_games.tsx
@@ -1,72 +1,77 @@
-import './top_games_styles.scss'
+import "./top_games_styles.scss";
 import { use, Suspense } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
-import NavBar from './navbar';
+import NavBar from "./navbar";
 
 function GlobalLeaderboard({ api_url }) {
-    const games = use(fetch(`${api_url}/leaders_board`).then(res => res.json()));
+  const games = use(
+    fetch(`${api_url}/leaders_board`).then((res) => res.json())
+  );
 
-    return (
-        <div className='content'>
-            {games.map(game => (
-                <Link className='game' to={`/games/${game.identifier}`}>
-                    {game.owner}
-                </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div className="content">
+      {games.map((game) => (
+        <Link className="game" to={`/games/${game.identifier}`}>
+          {game.owner}
+        </Link>
+      ))}
+    </div>
+  );
 }
 
 function YourGames({ api_url }) {
-    const [user, loading] = useAuthState(auth);
-    if(!user){return};
-    const jwt = user.accessToken;
-    const games = use(fetch(`${api_url}/games`, {
-        headers: {
-            authorization: `Bearer ${jwt}`,
-            'content-type': 'application/json'
-        },
-    }).then(res => res.json()));
+  const [user, loading] = useAuthState(auth);
+  if (!user) {
+    return;
+  }
+  const jwt = user.accessToken;
+  const games = use(
+    fetch(`${api_url}/games`, {
+      headers: {
+        authorization: `Bearer ${jwt}`,
+        "content-type": "application/json",
+      },
+    }).then((res) => res.json())
+  );
 
-    return (
-        <div className='content'>
-            {games.map(game => (
-                <Link className='game' to={`/games/${game.identifier}`}>
-                    {game.owner}
-                </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div className="content">
+      {games.map((game) => (
+        <Link className="game" to={`/games/${game.identifier}`}>
+          {game.owner}
+        </Link>
+      ))}
+    </div>
+  );
 }
-
 
 export default function TopGames() {
-    const api_url = "http://10.10.227.65:8000";
+  const api_url = import.meta.env.VITE_API_URL;
 
-    return (
-        <>
-            <NavBar />
-            <div className='top_games'>
-                <div className='rating'>
-                    <div className='heading'>top site games</div>
-                    <Suspense fallback={<span className='loading'>Loading...</span>}>
-                        <GlobalLeaderboard api_url={api_url} />
-                    </Suspense>
-                </div>
-                <div className='buttons'>
-                    <button className='button'>Single</button>
-                    <button className='button'>Multiple</button>
-                </div>
-                <div className='rating'>
-                    <div className='heading'>top your games</div>
-                    <Suspense fallback={<span className='loading'>Loading...</span>}>
-                        <YourGames api_url={api_url} />
-                    </Suspense>
-                </div>
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <div className="top_games">
+        <div className="rating">
+          <div className="heading">top site games</div>
+          <Suspense fallback={<span className="loading">Loading...</span>}>
+            <GlobalLeaderboard api_url={api_url} />
+          </Suspense>
+        </div>
+        <div className="buttons">
+          <button className="button">Single</button>
+          <button className="button">Multiple</button>
+        </div>
+        <div className="rating">
+          <div className="heading">top your games</div>
+          <Suspense fallback={<span className="loading">Loading...</span>}>
+            <YourGames api_url={api_url} />
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
 }
+
